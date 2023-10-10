2ac731de9ec757b9ad526912be8bf457fc1da566
Adrien Felipe <adrienfelipe@microsoft.com>
2023-10-10 22:45:16 +0200

    Code run draft

--- src/actions/run_code.py
+++ src/actions/run_code.py
@@ -1,5 +1,6 @@
 from action import Action
 from action_result import ActionResult
+from agent_proxy import AgentProxy
 from registry import action_registry
 from task import Task
 
@@ -8,7 +9,14 @@ class RunCode(Action):
     description = "Execute a single atomic code function"
 
     def execute(self, task: Task, reason: str = "") -> ActionResult:
-        """Apply the action given a task and a reason."""
+        if not task.plan:
+            code = AgentProxy.ask_for_code(task, reason)
+            
+        # TODO: validate code is not harmful
+        # TODO: test the code on test data
+        
+        # Eval run code
+        
 
 
 action_registry.register_action("run_code", RunCode)
