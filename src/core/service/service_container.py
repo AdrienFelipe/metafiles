2733fb6ee87643c23233e235ba8801cf1f02cc75
Adrien Felipe <adrienfelipe@microsoft.com>
2023-11-25 00:30:23 +0100

    Services as singletons

--- src/core/service/service_container.py
+++ src/core/service/service_container.py
@@ -5,8 +5,15 @@ from core.service.service_type import IService, TService
 
 class ServiceContainer:
     def __init__(self, services_registry: Dict[Type[IService], Type[TService]]):
-        self.services_registry = services_registry
+        self._services_registry = services_registry
+        self._instances: Dict[Type[TService], TService] = {}
 
     def get_service(self, service_key: Type[TService]) -> TService:
-        service_class = self.services_registry[service_key]
+        if service_key not in self._instances:
+            self._instances[service_key] = self._build_service(service_key)
+
+        return self._instances[service_key]
+
+    def _build_service(self, service_key: Type[TService]) -> TService:
+        service_class = self._services_registry[service_key]
         return service_class()
